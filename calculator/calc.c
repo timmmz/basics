dd0015c8ba6d390ef328d6dd5d06dcb74a45b52b
timmmz <huguet.tim34@gmail.com>
2025-07-09 15:09:53 +0200

    Support Add and Sub

--- calculator/calc.c
+++ calculator/calc.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
 
-    int myNum;
-    while (1 == 1){
-        // get user input
-        printf("Enter the number which you want to see doubled\n");
-        scanf("%d",&myNum);
-        int doubled = myNum * 2;
-        printf("Your number doucle is %d\n",doubled);
-        // return sum
+int main(int argc, char *argv[]){
+    // if too many arguments, print correct way and return 1
+    if (!argc == 4){
+        printf("Format is incorrect \n");
+        return 1;
     }
-    printf("Hello, World!\n");
+    // set the variables for the first el, second and third
+    int a = atoi(argv[1]);
+    int b = atoi(argv[3]);
+    char *op = argv[2];
+
+    // check if operator is correct
+    if (strcmp(op,"+") == 0) {
+        printf("Result = %d\n",a + b);
+    } else if (strcmp(op,"-") == 0) {
+        printf("Result = %d\n",a - b);
+    } else {
+        printf("Incorrect operator, please verify");
+        return 1;
+    }
+        // return result
+
+
+    // print result
     return 0;
 }
