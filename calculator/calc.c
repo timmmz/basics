e3714ee9c0d80125b9d352027f08243d33128994
timmmz <huguet.tim34@gmail.com>
2025-07-09 10:16:06 +0200

    Basic number doubling

--- calculator/calc.c
+++ calculator/calc.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
+
+    int myNum;
+    while (1 == 1){
+        // get user input
+        printf("Enter the number which you want to see doubled\n");
+        scanf("%d",&myNum);
+        int doubled = myNum * 2;
+        printf("Your number doucle is %d\n",doubled);
+        // return sum
+    }
     printf("Hello, World!\n");
     return 0;
 }
