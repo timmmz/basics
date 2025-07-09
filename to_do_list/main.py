821d1c8b6a467f35de3701940e48919dc010f75f
timmmz <huguet.tim34@gmail.com>
2025-07-09 16:11:15 +0200

    Add buggy update

--- to_do_list/main.py
+++ to_do_list/main.py
@@ -12,7 +12,10 @@ def connect():
 # it should be a cli interface that ask whether to create, read, update or remove
 # CRUD
 # Create task
-def create_task(conn,cursor,task_name,task_description):
+def create_task(conn,cursor):
+    
+    task_name = input("Please enter task's name : ")
+    task_description = input("Please enter task's content : ")
     cursor.execute("INSERT INTO tasks(task_name,task_description) VALUES(?,?)",(task_name,task_description))
     conn.commit()
     print("Task added to database")
@@ -24,6 +27,15 @@ def display_tasks(cursor):
         print(f"{id}-- {task_name} -- {task_description}")
     
 # Update Tasks
+def update_task(conn,cursor):
+    #get the content of the task by id
+    task_id = int(input("Please enter id: "))
+    cursor.execute("SELECT task_name,task_description FROM tasks WHERE id=?",(task_id,))
+    task = cursor.fetchone()
+    delete_task(conn,cursor,task_id)
+    create_task(conn,cursor)
+    # delete the task 
+    # create the task
 # Delete task
 def delete_task(conn,cursor,task_id):
     cursor.execute("DELETE FROM tasks WHERE id=?",(task_id,))
@@ -47,14 +59,14 @@ def main():
         if user_input == 'q':
             break
         if user_input == '1':
-            task_name = input("Please enter task's name : ")
-            task_description = input("Please enter task's content : ")
-            create_task(conn,cursor,task_name,task_description)
+            create_task(conn,cursor)
         if user_input == "2":
             display_tasks(cursor)
         if user_input == "3":
             task_id = int(input("which task ? : "))
             delete_task(conn,cursor,task_id)
+        if user_input == "4":
+            update_task(conn,cursor)
             
     conn.close()
     return
